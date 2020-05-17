4f986784b4dcf8f1f4c6e31b8ca99727a7ae19bd
Khương Văn Hùng <46420538+khuonghung1423@users.noreply.github.com>
2020-05-17 17:02:29 +0700

    TARGET_SUPPORTS_GOOGLE_RECORDER set to false

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -22,6 +22,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_WIFI_EXT := true
 TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_ginkgo
