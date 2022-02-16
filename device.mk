fe615ef22dde181ca6df9add0705599881b03a8b
Jakub Pawlowski <jpawlowski@google.com>
2022-02-16 09:07:03 -0800

    gingko: Use bluetooth.audio@2.1 HAL
    This is needed for HalImplementationTest
    
    Bug: 150670922
    Test: this keeps HalImplementationTest passing.
    Change-Id: I9df5591eb9ef2af49f4daec4238d84a5481cd842
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -74,7 +74,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audio.bluetooth.default \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libbtconfigstore \
