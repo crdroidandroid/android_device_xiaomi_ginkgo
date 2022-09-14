19b34eb257e70b06e90b6bd75d21453012ac9c2f
tarsin <yuanqingxiang233@163.com>
2022-09-14 18:46:23 +0200

    ginkgo: shims: Make shim for miwatermark.so
    Logs:
    vndksupport: Could not load /vendor/lib/hw/camera.trinket.so from default namespace: dlopen failed: cannot locate symbol "_ZN4piex19GetPreviewImageDataEPNS_15StreamInterfaceEPNS_16PreviewImageDataE" referenced by "/vendor/lib/miwatermark.so"....
    
    Change-Id: I47fea33432692a7a76d71db91676cb3a58ac19c7

--- device.mk
+++ device.mk
@@ -98,7 +98,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     android.hidl.memory.block@1.0.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    libwatermark_shim
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
