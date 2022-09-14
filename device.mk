1db238d2e573c77dd9cb29f4d14acf18e52848bd
DarkJoker360 <simoespo159@gmail.com>
2022-09-14 18:50:11 +0200

    ginkgo: Build android.frameworks.sensorservice@1.0
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -389,6 +389,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.0-service.multihal \
     vendor.qti.hardware.display.mapper@1.1.vendor
 
