457fb4999bb34232eedd0bd572b9cb62a82aa180
dianlujitao <dianlujitao@lineageos.org>
2022-10-07 20:29:49 +0800

    ginkgo: Nuke ATFWD It's normally not used at all.
    Change-Id: Idb824b3b8c783b6ac962e604df05f9154beb4ed2
    Signed-off-by: mnasibzade <euoryexe@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,7 +92,6 @@ USE_OPENGL_RENDERER := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     vendor/alpha/config/device_framework_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 ODM_MANIFEST_WILLOW_FILES := $(DEVICE_PATH)/configs/hidl/manifest_willow.xml
