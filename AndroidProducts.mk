158c57869484c2f4671a49d4f29827dfab37065d
otispl <wgwszymon@gmail.com>
2022-12-13 17:14:41 +0000

    ginkgo: Time for tequila!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ginkgo.mk
+    $(LOCAL_DIR)/tequila_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ginkgo-user \
-    lineage_ginkgo-userdebug \
-    lineage_ginkgo-eng
+    tequila_ginkgo-user \
+    tequila_ginkgo-userdebug \
+    tequila_ginkgo-eng
--- device.mk
+++ device.mk
@@ -14,7 +14,7 @@ $(call inherit-product, vendor/xiaomi/ginkgo/ginkgo-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-tequila
 
 PRODUCT_PACKAGES += \
     GinkgoNoCutoutOverlay \
