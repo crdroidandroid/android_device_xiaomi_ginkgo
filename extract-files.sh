9c4f9e900cb345364d0d7dd69c23d885378a54ee
Tim Zimmermann <tim@linux4.de>
2023-10-16 22:21:49 +0000

    ginkgo: Patch widevine to load libprotobuf-cpp-full-3.9.1
    * Patch widevine to load full variant as there's no lite vendorcompat variant
    
    Change-Id: I8d1fbc40bebf66abea342780b02cda68d64899b1
    Signed-off-by: tejas101k <tejassingh649@rediffmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -62,6 +62,12 @@ function blob_fixup() {
         vendor/lib/miwatermark.so)
             "${PATCHELF}" --add-needed "libwatermark_shim.so" "${2}"
             ;;
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
+        vendor/lib64/mediadrm/libwvdrmengine.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
+            ;;
     esac
 }
 
