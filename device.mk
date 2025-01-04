49d3e24431fc7d8136d20d4b8d12977b05333c9b
Michael Bestas <mkbestas@lineageos.org>
2025-01-04 09:09:32 +0200

    ginkgo: Remove OMX audio codecs
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on Lineage.
     * OMX SW audio codecs no longer exist.
    
    Change-Id: I940be6716b603e848c44839bb87ad690eb55e86f
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>#
    Signed-off-by: mnasibzade <euoryexe@gmail.com>

--- device.mk
+++ device.mk
@@ -259,11 +259,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libOmxG711Enc \
