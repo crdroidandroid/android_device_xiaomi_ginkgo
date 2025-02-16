4fa9fea06deab39efdf8427019f3bcc439c195c8
ZorEl212 <yabsirad212@gmail.com>
2025-02-16 01:44:53 -0500

    ginkgo: Move to our own `CameraProvider2.4-impl`
    Due to changes introduced on [1] and [2] camera hal completely crashes.
    Override default CameraProvider2.4-impl and keep it to point before these changes.
    
    [1]: https://github.com/crdroidandroid/android_hardware_interfaces/commit/8526b6973dc011316915bf99ab6616f463c4b0e8
    
    [2]: https://github.com/crdroidandroid/android_hardware_interfaces/commit/8d5bf376c657f0c5fd0b6bb65e38066e07d7152b
    
    Signed-off-by: ZorEl212 <yabsirad212@gmail.com>
    Signed-off-by: mnasibzade <euoryexe@gmail.com>

--- device.mk
+++ device.mk
@@ -94,7 +94,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
-    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-impl-ginkgo \
     android.hardware.camera.provider@2.4-service \
     android.hidl.memory.block@1.0.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
